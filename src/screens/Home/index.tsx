2ba786a98cbf48145422163a01c6a3d33245986d
Kauã Steliano <stelianok@gmail.com>
2021-02-13 11:35:46 -0300

    Header finalizado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,62 @@
 import React from 'react';
-import {Container} from './styles';
+
+import {
+  Container,
+  Header,
+  HeaderTop,
+  LogoutButton,
+  SearchBarContainer,
+  SearchBarInput,
+  ClassesContainer,
+  InfoView,
+  Category,
+  Courses,
+  Classes,
+  Class,
+  ClassLogo,
+  Title,
+  Lessons,
+} from './styles';
+
+import logo from '../../assets/img/Logotipo.png';
+import math from '../../assets/img/Math.png';
+
+import Icon from 'react-native-vector-icons/Feather';
+import {Image} from 'react-native';
 
 const Home: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <HeaderTop>
+          <Image source={logo} />
+          <LogoutButton>
+            <Icon name={'power'} size={30} color={'#FF6680'} />
+          </LogoutButton>
+        </HeaderTop>
+        <SearchBarContainer>
+          <Icon name={'search'} size={23} color={'#C4C4D1'} />
+          <SearchBarInput
+            placeholder={'Busque um curso'}
+            placeholderTextColor={'#C4C4D1'}
+          />
+        </SearchBarContainer>
+      </Header>
+      <ClassesContainer>
+        <InfoView>
+          <Category>Categorias</Category>
+          <Courses>43 cursos</Courses>
+        </InfoView>
+        <Classes>
+          <Class>
+            <ClassLogo source={math} />
+            <Title>Matemática</Title>
+            <Lessons>16 aulas</Lessons>
+          </Class>
+        </Classes>
+      </ClassesContainer>
+    </Container>
+  );
 };
 
 export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,2 +1,50 @@
 import styled from 'styled-components/native';
-export const Container = styled.View``;
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  flex: 0.25;
+  margin: 25px;
+`;
+export const HeaderTop = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+export const LogoutButton = styled.TouchableOpacity`
+  margin-bottom: 5px;
+`;
+export const SearchBarContainer = styled.View`
+  background-color: #fff;
+
+  height: 60px;
+
+  flex-direction: row;
+
+  border-radius: 50px;
+
+  justify-content: space-between;
+  align-items: center;
+
+  padding-left: 20px;
+`;
+export const SearchBarInput = styled.TextInput`
+  flex: 0.95;
+`;
+export const ClassesContainer = styled.View`
+  flex: 1;
+  background-color: #f0edf5;
+  border-top-left-radius: 25px;
+  border-top-right-radius: 25px;
+`;
+export const InfoView = styled.View``;
+export const Category = styled.Text``;
+export const Courses = styled.Text``;
+export const Classes = styled.View``;
+export const Class = styled.View``;
+export const ClassLogo = styled.Image``;
+export const Title = styled.Text``;
+export const Lessons = styled.Text``;
