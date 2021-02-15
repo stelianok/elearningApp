ad9caa64550b62e3663448a549fb1eeb185fd811
Kauã Steliano <stelianok@gmail.com>
2021-02-15 12:44:19 -0300

    Header estilizado, estrutura criada.

--- src/screens/Lectures/index.tsx
+++ src/screens/Lectures/index.tsx
@@ -1,8 +1,77 @@
 import React from 'react';
-import {Container} from './styles';
+import {Image} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {useNavigation} from '@react-navigation/native';
+
+import logo from '../../assets/img/Logotipo.png';
+
+import {
+  Container,
+  Header,
+  HeaderTop,
+  GoBackButton,
+  FavoriteButton,
+  LectureContainer,
+  InfoView,
+  Title,
+  NumberOfLectures,
+  Lectures,
+  Lecture,
+  PlayButtonView,
+  LectureInformation,
+  LectureTitle,
+  LectureSubtitleContainer,
+  LectureSubtitle,
+  LectureDurationContainer,
+  LectureDuration,
+} from './styles';
 
 const VideoLecture: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <Header>
+        <HeaderTop>
+          <GoBackButton
+            onPress={() => {
+              navigation.goBack();
+            }}>
+            <Icon name={'arrow-left'} size={30} color={'#FF6680'} />
+          </GoBackButton>
+          <Image source={logo} />
+          <FavoriteButton
+            onPress={() => {
+              navigation.navigate('Login');
+            }}>
+            <Icon name={'power'} size={30} color={'#FF6680'} />
+          </FavoriteButton>
+        </HeaderTop>
+        <LectureContainer>
+          <InfoView>
+            <Title>Matemática</Title>
+            <NumberOfLectures>16 aulas</NumberOfLectures>
+          </InfoView>
+          <Lectures>
+            <Lecture>
+              <PlayButtonView>
+                <Icon name={'play-circle'} size={30} color={'#fff'} />
+              </PlayButtonView>
+              <LectureInformation>
+                <LectureTitle>Introdução à teoria matemática</LectureTitle>
+                <LectureSubtitleContainer>
+                  <LectureSubtitle> Aula 03</LectureSubtitle>
+                  <LectureDurationContainer>
+                    <Icon name={'clock'} size={30} color={'#C4C4D1'} />
+                    <LectureDuration>5min</LectureDuration>
+                  </LectureDurationContainer>
+                </LectureSubtitleContainer>
+              </LectureInformation>
+            </Lecture>
+          </Lectures>
+        </LectureContainer>
+      </Header>
+    </Container>
+  );
 };
 
 export default VideoLecture;
--- src/screens/Lectures/styles.ts
+++ src/screens/Lectures/styles.ts
@@ -1,2 +1,47 @@
 import styled from 'styled-components/native';
-export const Container = styled.View``;
+export const Container = styled.View`
+  background-color: #6548a3;
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  flex: 0.25;
+  margin: 25px;
+  margin-bottom: 50px;
+`;
+export const HeaderTop = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
+export const GoBackButton = styled.TouchableOpacity``;
+
+export const FavoriteButton = styled.TouchableOpacity`
+  margin-bottom: 5px;
+`;
+
+export const LectureContainer = styled.View`
+  flex: 1;
+  background-color: #f0edf5;
+  border-top-left-radius: 25px;
+  border-top-right-radius: 25px;
+
+  padding: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+`;
+
+export const InfoView = styled.View``;
+export const Title = styled.Text``;
+export const NumberOfLectures = styled.Text``;
+export const Lectures = styled.ScrollView``;
+export const Lecture = styled.View``;
+export const PlayButtonView = styled.View``;
+export const LectureInformation = styled.View``;
+export const LectureTitle = styled.Text``;
+export const LectureSubtitleContainer = styled.View``;
+export const LectureSubtitle = styled.Text``;
+export const LectureDurationContainer = styled.View``;
+export const LectureDuration = styled.Text``;
