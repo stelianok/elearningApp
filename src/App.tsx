a14c50e737daf9b77b11368e8b92ecf4db83e759
Kauã Steliano <stelianok@gmail.com>
2021-02-16 11:13:13 -0300

    configurações adicionais de splashscreen adicionadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppRoutes from './routes/app.routes';
 import {NavigationContainer} from '@react-navigation/native';
+
 const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <StatusBar backgroundColor="#6548A3" />
