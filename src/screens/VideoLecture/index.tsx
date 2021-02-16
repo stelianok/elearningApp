29fc1ccf2a6293c52ebe8d131ab5696f9f73c8eb
Kauã Steliano <stelianok@gmail.com>
2021-02-16 08:29:25 -0300

    Tela com video completamente implementada.

--- src/screens/VideoLecture/index.tsx
+++ src/screens/VideoLecture/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, useWindowDimensions} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import {useNavigation} from '@react-navigation/native';
+import Video from 'react-native-video';
 
 import logo from '../../assets/img/Logotipo.png';
+import testVideo from '../../assets/video/FeverTheGhostSource.mp4';
+
 import {
   Container,
   Header,
@@ -11,10 +14,26 @@ import {
   GoBackButton,
   FavoriteButton,
   ContentContainer,
+  VideoContainer,
+  VideoInformation,
+  VideoInformationHeader,
+  Title,
+  SubtitleContainer,
+  LectureNumber,
+  LectureDurationContainer,
+  LectureDuration,
+  VideoDescription,
+  ChangeLectureButtonContainer,
+  PreviousLectureButton,
+  PreviousLectureButtonText,
+  NextLectureButton,
+  NextLectureButtonText,
 } from './styles';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const VideoLecture: React.FC = () => {
   const navigation = useNavigation();
+  const width = useWindowDimensions().width;
   return (
     <Container>
       <Header>
@@ -31,7 +50,63 @@ const VideoLecture: React.FC = () => {
           </FavoriteButton>
         </HeaderTop>
       </Header>
-      <ContentContainer />
+      <ContentContainer>
+        <VideoContainer>
+          <Video
+            source={testVideo}
+            controls={true}
+            resizeMode={'cover'}
+            poster={'https://i.ytimg.com/vi/9RHFFeQ2tu4/maxresdefault.jpg'}
+            posterResizeMode={'contain'}
+            style={{
+              width,
+              height: 250,
+              borderTopLeftRadius: 15,
+              borderTopRightRadius: 15,
+            }}
+          />
+        </VideoContainer>
+
+        <ScrollView>
+          <VideoInformation>
+            <VideoInformationHeader>
+              <Title>Introdução a teoria matemática</Title>
+              <SubtitleContainer>
+                <LectureNumber>Aula 01</LectureNumber>
+                <LectureDurationContainer>
+                  <Icon name={'clock'} size={20} color={'#A0A0B2'} />
+                  <LectureDuration>5 Min</LectureDuration>
+                </LectureDurationContainer>
+              </SubtitleContainer>
+            </VideoInformationHeader>
+            <VideoDescription>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+              eaque esse expedita odio iure ducimus nam soluta nihil itaque,
+              consectetur, quas, recusandae ex sed dolor placeat molestiae
+              delectus eveniet. Iste! Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Nostrum eaque esse expedita odio iure ducimus
+              nam soluta nihil itaque, consectetur, quas, recusandae ex sed
+              dolor placeat molestiae delectus eveniet. Iste!Lorem ipsum dolor,
+            </VideoDescription>
+            <VideoDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sequi
+              autem natus officia. Laudantium eveniet distinctio mollitia beatae
+              ab sequi voluptatum similique optio hic itaque culpa, ullam eos
+              magnam nobis.
+            </VideoDescription>
+          </VideoInformation>
+        </ScrollView>
+        <ChangeLectureButtonContainer>
+          <PreviousLectureButton>
+            <Icon name={'arrow-left'} size={20} color={'#FF6680'} />
+            <PreviousLectureButtonText>Aula anterior</PreviousLectureButtonText>
+          </PreviousLectureButton>
+          <NextLectureButton>
+            <Icon name={'arrow-right'} size={20} color={'#FFF'} />
+            <NextLectureButtonText>Próxima aula</NextLectureButtonText>
+          </NextLectureButton>
+        </ChangeLectureButtonContainer>
+      </ContentContainer>
     </Container>
   );
 };
--- src/screens/VideoLecture/styles.ts
+++ src/screens/VideoLecture/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-
+import {RectButton} from 'react-native-gesture-handler';
 export const Container = styled.View`
   background-color: #6548a3;
   flex: 1;
@@ -8,7 +8,7 @@ export const Container = styled.View`
 export const Header = styled.View`
   flex: 0.25;
   margin: 25px;
-  margin-bottom: -90px;
+  margin-bottom: -120px;
 `;
 export const HeaderTop = styled.View`
   flex-direction: row;
@@ -26,5 +26,103 @@ export const FavoriteButton = styled.TouchableOpacity`
 
 export const ContentContainer = styled.View`
   flex: 1;
-  border-radius: 15px;
+
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #f0edf5;
+
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+`;
+
+export const VideoContainer = styled.View`
+  background-color: #3d3d4c;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+`;
+
+export const VideoInformation = styled.View`
+  flex: 0.91;
+
+  margin: 20px;
+`;
+export const VideoInformationHeader = styled.View``;
+export const Title = styled.Text`
+  font-family: 'Rubik';
+  font-size: 30px;
+  color: #3d3d4c;
+  margin-bottom: 15px;
+`;
+export const SubtitleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  margin-bottom: 25px;
+`;
+export const LectureNumber = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 14px;
+  color: #a0a0b2;
+
+  margin-right: 25px;
+`;
+export const LectureDurationContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const LectureDuration = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 14px;
+  color: #a0a0b2;
+
+  margin-left: 12px;
+`;
+export const VideoDescription = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 18px;
+  text-align: left;
+  line-height: 30px;
+  color: #6c6c80;
+`;
+export const ChangeLectureButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+export const PreviousLectureButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+
+  margin-right: 20px;
+`;
+export const PreviousLectureButtonText = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 15px;
+  color: #ff6680;
+
+  margin-left: 15px;
+  margin-right: 25px;
+`;
+export const NextLectureButton = styled(RectButton)`
+  flex-direction: row-reverse;
+  background-color: #ff6680;
+
+  justify-content: center;
+  align-items: center;
+
+  padding: 20px;
+  padding-left: 27px;
+  padding-right: 27px;
+
+  border-radius: 50px;
+`;
+export const NextLectureButtonText = styled.Text`
+  font-family: 'Roboto-Regular';
+  margin-right: 15px;
+
+  font-size: 15px;
+  color: #fff;
 `;
